895593da90cdcfb0fcff29509b57eec201d9d709
KarenMatoke <karen.matoke@snyk.io>
2022-07-04 22:39:22 +0100

    safestring call test

---
+++ auth02.cs
@@ -0,0 +1,21 @@
+public async (Task<AuthenticateResponse> RegisterAsynch(string email, string password, string x)
+{
+string sendEmail = email.ToSafeString();
+_logger.LogInformation($"Attempting registration for user {sendEmail}");
+AuthenticateResponse result = new();
+APIClient.RegisterRequest request = new()
+{
+Email = email,
+Password = password,
+Ident= ident
+};
+try
+{
+result = await apiClient.RegisterAsync(request);
+}
+catch { }
+if (result != null && !string.IsNullOrEmpty(result.Token))
+_logger.LogInformation($"Attempted registration for user {sendEmail} SUCCESSFUL")
+else
+{};
+}
